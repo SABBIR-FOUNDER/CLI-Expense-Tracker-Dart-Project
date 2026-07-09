be618175ab1c109b3e4441eeae2f8ad6f94a48a0
SABBIR-FOUNDER <sabbir911911@gmail.com>
2026-07-10 01:48:32 +0600

    3rd commit

--- bin/cli_expense_tracker.dart
+++ bin/cli_expense_tracker.dart
@@ -5,3 +5,16 @@ extension CurrencyFormatter on double{
   }
 }
 
+abstract class Expense {
+  final String _title;
+  final double _amount;
+  final String _catagory;
+
+  Expense(this._title, this._amount, this._catagory);
+
+  String get title =>_title;
+  double get amount=>_amount;
+  String get catagory=> _catagory;
+
+
+}
